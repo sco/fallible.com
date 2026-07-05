58cb582022c73e33aa245a55457639542db082d4
draymondkc <draymondkc@gmail.com>
2026-07-05 18:20:03 -0500

    commit
    commit

---
+++ Marginal - Christian, Blog, Doug Raymond, Kansas City, Missouri_files/partner.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+
+}
+/*
+     FILE ARCHIVED ON 14:23:45 Mar 02, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:56:06 Aug 12, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.44
+  exclusion.robots: 0.017
+  exclusion.robots.policy: 0.008
+  esindex: 0.01
+  cdx.remote: 1484.215
+  LoadShardBlock: 223.745 (3)
+  PetaboxLoader3.datanode: 99.912 (4)
+  PetaboxLoader3.resolve: 290.756 (3)
+  load_resource: 187.426
+*/
